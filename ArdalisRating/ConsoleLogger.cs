f4d9850c0d6518564c25d73664f26d6cecf206b3
Csernak Tamas <altvik26@gmail.com>
2020-08-13 22:47:53 +0200

    introduced ConsoleLogger

---
+++ ArdalisRating/ConsoleLogger.cs
@@ -0,0 +1,10 @@
+using System;
+using System.IO;
+
+namespace ArdalisRating {
+    public class ConsoleLogger {
+        public void Log(string message){
+            Console.WriteLine(message);
+        }
+    }   
+}
